4f6a5cc9bed0b702904029f590a781e032e233e2
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-03-08 19:43:22 +0100

    add create-account component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 export const routes: Routes = [
-    {path: '', component: LogInComponent}
+    {path: '', component: LogInComponent},
+    {path: 'create-account', component: CreateAccountComponent}
 ];
