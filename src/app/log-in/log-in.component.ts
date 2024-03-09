98aad6ae552ab6526f970e0081c2e15c71b7f648
Jad El Nader <Jadelnader99@gmail.com>
2024-03-09 12:24:45 +0100

    matcard

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [IntroComponent],
+  imports: [IntroComponent, MatCardModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
