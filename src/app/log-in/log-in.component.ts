d32d68269e576ff7db84e3d41d6f447894e10423
Jad El Nader <Jadelnader99@gmail.com>
2024-03-09 10:58:56 +0100

    create intro component

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IntroComponent } from './intro/intro.component';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [IntroComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
