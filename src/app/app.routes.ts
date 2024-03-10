b596dd080743d3c98f76efce615e2b76cfcd44f2
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-03-10 15:25:03 +0100

    add Logo as favicon, add select-avatar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { SelectAvatarComponent } from './select-avatar/select-avatar.component';
 
 export const routes: Routes = [
     {path: '', component: LogInComponent},
-    {path: 'create-account', component: CreateAccountComponent}
+    {path: 'create-account', component: CreateAccountComponent},
+    {path: 'select-avatar', component: SelectAvatarComponent}
 ];
---
+++ src/app/select-avatar/select-avatar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-select-avatar',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './select-avatar.component.html',
+  styleUrl: './select-avatar.component.scss'
+})
+export class SelectAvatarComponent {
+
+}
