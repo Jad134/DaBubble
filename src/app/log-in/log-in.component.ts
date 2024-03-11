81de46f1d4cfea0767fd9eff34e3e8056f3f4cbb
Jad El Nader <Jadelnader99@gmail.com>
2024-03-11 18:53:21 +0100

    add formvalidate for email error message

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [IntroComponent, MatCardModule, RouterModule],
+  imports: [IntroComponent, MatCardModule, RouterModule, FormsModule, ReactiveFormsModule,],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
+  @ViewChild('mailField') mailField!: ElementRef;
+  @ViewChild(' passwordMessage') passwordMessage!: ElementRef;
 
+
+
+
+  logIn() {
+    this.validateCheck()
+    
+
+  }
+
+  validateCheck(){
+    const mailfield = this.mailField.nativeElement;
+    const passwordMessage = this.passwordMessage.nativeElement;
+
+    if (!mailfield.validity.valid) {
+      passwordMessage.style = 'opacity: 1'
+    } else {
+      passwordMessage.style = 'opacity: 0'
+    }
+  }
 }
