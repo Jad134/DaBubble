eeda1176be85a376652723ebd1072724b45499d7
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-03-11 20:19:18 +0100

    create Account; insert Validation for name, mail and psw (only basic)

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, FormsModule, CommonModule],
   templateUrl: './create-account.component.html',
-  styleUrl: './create-account.component.scss'
+  styleUrl: './create-account.component.scss',
 })
 export class CreateAccountComponent {
+  newUser = new User();
+  nameFailed = false;
+  mailFailed = false;
+  pswFailed = false;
 
+  createAccount() {
+    if (this.checkNameInputNotEmpty()) {
+      console.log(this.newUser.name);
+    }
+    if (this.checkIfCorrectMailFormat()) {
+      console.log(this.newUser.eMail);
+    }
+    if (this.checkCorrectPasswordFormat()) {
+      console.log(this.newUser.password);
+    }
+  }
+
+  checkNameInputNotEmpty() {
+    this.nameFailed = this.newUser.name.length <= 4;
+    return !this.nameFailed;
+  }
+
+  checkIfCorrectMailFormat() {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    this.mailFailed = !(
+      this.newUser.eMail.match(emailPattern) && this.newUser.eMail.length > 6
+    );
+    return !this.mailFailed;
+  }
+
+  checkCorrectPasswordFormat() {
+    this.pswFailed = this.newUser.password.length < 8;
+    return !this.pswFailed;
+  }
 }
--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,3 +1,14 @@
 export class User{
-    
+    name: string;
+    eMail: string;
+    password: string;
+    avatar: any;
+
+    constructor(obj? : any){
+        this.name = obj ? obj.name : '';
+        this.eMail = obj ? obj.eMail : '';
+        this.password = obj ? obj.password : '';
+        this.avatar = obj ? obj.avatar : '';
+    }
+
 }
