8a6267ffd4c0c7393b302336df0ca8b4852c9e6e
Jad El Nader <Jadelnader99@gmail.com>
2024-03-14 13:28:42 +0100

    add clicket avatar to db

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -7,7 +7,7 @@ import { Auth, GoogleAuthProvider } from '@angular/fire/auth'
 import { Router } from '@angular/router';
 import { routes } from '../app.routes';
 import { User } from '../../models/user.class';
-import { doc, setDoc, collection } from "firebase/firestore";
+import { doc, setDoc, collection, updateDoc } from "firebase/firestore";
 
 
 @Injectable({
@@ -17,7 +17,7 @@ import { doc, setDoc, collection } from "firebase/firestore";
 
 export class FirestoreService {
 
- logInInvalid = false;
+  logInInvalid = false;
 
   firebaseConfig = {
     apiKey: "AIzaSyDiGmIlzMq2kQir6-xnHFX9iOXxH1Wcj8o",
@@ -38,96 +38,106 @@ export class FirestoreService {
   users = new User;
 
 
-    constructor(private router: Router, public ngZone: NgZone) { }
+  constructor(private router: Router, public ngZone: NgZone) { }
 
   //----------------------------------------Create Account------------------------------------------
-  async createUserWithEmailAndPassword(email: string, password: string, userDatas: any): Promise < void> {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
-    const user = userCredential.user;
-    let userid = user.uid
-        // Erstellen Sie ein neues Dokument in der Firestore-Sammlung 'users'
-        await setDoc(doc(this.db, "Users", userid), {
-          name: userDatas.name,
-          email: userDatas.eMail,
-          avatar: userDatas.avatar,
-        });
+  async createUserWithEmailAndPassword(email: string, password: string, userDatas: any): Promise<void> {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
+      const user = userCredential.user;
+      let userid = user.uid
+      // Erstellen Sie ein neues Dokument in der Firestore-Sammlung 'users'
+      await setDoc(doc(this.db, "Users", userid), {
+        name: userDatas.name,
+        email: userDatas.eMail,
+        avatar: userDatas.avatar,
+      });
 
-  // Weiterleitung zur Dashboard-Seite
-  this.ngZone.run(() => {
-    // Automatic forwarding to the Select Avatar component after successful creation of a new user
-    this.router.navigate(['/select-avatar/' + userid]);
-  });
-} catch (error) {
-  console.error('Fehler beim Erstellen des Benutzers:', error);
-}
+      // Weiterleitung zur Dashboard-Seite
+      this.ngZone.run(() => {
+        // Automatic forwarding to the Select Avatar component after successful creation of a new user
+        this.router.navigate(['/select-avatar/' + userid]);
+      });
+    } catch (error) {
+      console.error('Fehler beim Erstellen des Benutzers:', error);
+    }
   }
 
 
   //-- ----------------------------------- ---Anmelden--------------------------------------------
   // https://www.linkedin.com/pulse/angular-14-firebase-authentication-tutorial-attia-imed/ daraus kommt die anmlede funktion
-   
- 
-  async Login(email : string, password : string){
-  try {
-    const result = await signInWithEmailAndPassword(this.auth, email, password);
-    const user = result.user;
-    let userid = user.uid;
-    this.ngZone.run(() => {
-      this.router.navigate(['/dashboard/' + userid]);
-      console.log(userid);
-      
-      this.logInInvalid = false;
-    });
-  } catch (error) {
-    window.alert('error, anmelden geht nicht');
-    this.logInInvalid = true;
+
+
+  async Login(email: string, password: string) {
+    try {
+      const result = await signInWithEmailAndPassword(this.auth, email, password);
+      const user = result.user;
+      let userid = user.uid;
+      this.ngZone.run(() => {
+        this.router.navigate(['/dashboard/' + userid]);
+        console.log(userid);
+
+        this.logInInvalid = false;
+      });
+    } catch (error) {
+      window.alert('error, anmelden geht nicht');
+      this.logInInvalid = true;
+    }
   }
-}
 
-//Login with Google
-GoogleAuth() {
-  return this.loginWithPopup(new GoogleAuthProvider());
-}
+  //Login with Google
+  GoogleAuth() {
+    return this.loginWithPopup(new GoogleAuthProvider());
+  }
 
-  async loginWithPopup(provider : any) {
-  return signInWithPopup(this.auth, provider).then(() => {
-    this.router.navigate(['dashboard']);
-  });
-}
+  async loginWithPopup(provider: any) {
+    return signInWithPopup(this.auth, provider).then(() => {
+      this.router.navigate(['dashboard']);
+    });
+  }
 
   //Send Password Reset Email
-    async sendPasswordResetEmails(email : string){
-      sendPasswordResetEmail(this.auth,email)
+  async sendPasswordResetEmails(email: string) {
+    sendPasswordResetEmail(this.auth, email)
       .then(() => {
-         window.alert('Password reset email sent, check your inbox.');
+        window.alert('Password reset email sent, check your inbox.');
       })
       .catch((error) => {
-       window.alert(error.message);
-     });
-   }
+        window.alert(error.message);
+      });
+  }
 
-   async getUser(id: string){
-    const unsub = onSnapshot(doc(this.firestore, 'users', id), (doc)=>{
+  async getUser(id: string) {
+    const unsub = onSnapshot(doc(this.firestore, 'users', id), (doc) => {
       return doc.data();
     });
   }
 
-   async getUserDataById(id: string) {
+  async getUserDataById(id: string) {
     try {
-        const docRef = doc(this.db, 'Users', id);
-        const docSnap = await getDoc(docRef);
-        
-        if (docSnap.exists()) {
-            return docSnap.data();
-        } else {
-            console.log('Kein Dokument mit dieser ID gefunden');
-            return null;
-        }
-    } catch (error) {
-        console.error('Fehler beim Abrufen des Dokuments:', error);
-        return null;
-    }
-}
+      const docRef = doc(this.db, 'Users', id);
+      const docSnap = await getDoc(docRef);
 
+      if (docSnap.exists()) {
+        return docSnap.data();
+      } else {
+        console.log('Kein Dokument mit dieser ID gefunden');
+        return null;
+      }
+    } catch (error) {
+      console.error('Fehler beim Abrufen des Dokuments:', error);
+      return null;
+    }
+  }
+
+
+  // --------------------------Update Db with avatar?--------------------------------------
+
+  async updateUser(id : string, avatarRef : string) {
+    const userRef = doc(this.db, "Users", id);
+    await updateDoc(userRef, {
+      avatar: avatarRef
+    });
+    console.log(id, avatarRef)
+  }
 }
