99fa1159d1c165d983aeb177d38bbcd43cefb9cc
Leon9708 <leon.ring@gmx.de>
2024-03-15 16:32:26 +0100

    add router

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { StartPageComponent } from '../start-page/start-page.component';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [MatCardModule, StartPageComponent, MatIconModule],
+  imports: [MatCardModule, StartPageComponent, MatIconModule, RouterModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
