b84fbcf4e2faff28680d69ee584be58029a24f99
Leon9708 <leon.ring@gmx.de>
2024-03-17 10:53:05 +0100

    seperat Component head dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeadDashboardComponent } from './head-dashboard/head-dashboard.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeadDashboardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
---
+++ src/app/dashboard/head-dashboard/head-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './head-dashboard.component.html',
+  styleUrl: './head-dashboard.component.scss'
+})
+export class HeadDashboardComponent {
+
+}
