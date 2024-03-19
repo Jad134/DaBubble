84445ba75188dd4a1188b43d93a9b29e578a869a
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-03-19 18:58:42 +0100

    thread-component: start designing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { NewPasswortComponent } from './log-in/reset-passwort/new-passwort/new-p
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { DataprotectionComponent } from './dataprotection/dataprotection.component';
+import { ThreadComponent } from './dashboard/thread/thread.component';
 
 export const routes: Routes = [
   { path: '', component: LogInComponent },
@@ -17,4 +18,5 @@ export const routes: Routes = [
   { path: 'dashboard/:id', component: DashboardComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'dataprotection', component: DataprotectionComponent },
+  { path: 'thread', component: ThreadComponent },
 ];
