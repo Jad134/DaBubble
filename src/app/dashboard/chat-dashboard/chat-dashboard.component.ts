b65a2a5442c9bcd7cfd879a167b0cd4cc333176c
Jad El Nader <Jadelnader99@gmail.com>
2024-03-26 11:09:57 +0100

    add emoji picker

--- src/app/dashboard/chat-dashboard/chat-dashboard.component.ts
+++ src/app/dashboard/chat-dashboard/chat-dashboard.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { PickerModule } from '@ctrl/ngx-emoji-mart'
 @Component({
   selector: 'app-chat-dashboard',
   standalone: true,
-  imports: [MatIconModule,MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, PickerModule],
   templateUrl: './chat-dashboard.component.html',
   styleUrl: './chat-dashboard.component.scss'
 })
 export class ChatDashboardComponent {
   color: boolean = false;
+  @ViewChild('emojiPicker') emojiPicker!: ElementRef;
+
+  addEmoji(event: any) {
+    // Implementiere hier die Logik zum Hinzufügen des ausgewählten Emojis
+    console.log('Ausgewähltes Emoji:', event);
+    // Beispiel: Fügen Sie das Emoji zum Eingabefeld hinzu
+  }
+
+  openEmojiPicker(){
+   
+  }
 }
