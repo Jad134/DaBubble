ca712d042f962f0e53e48c3db991c8b394a922d0
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-04-03 20:17:48 +0200

    edit-profile-dialog: insert user image

--- src/app/dashboard/head-dashboard/edit-profile-dialog/edit-profile-dialog.component.ts
+++ src/app/dashboard/head-dashboard/edit-profile-dialog/edit-profile-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatCard } from '@angular/material/card';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Router } from 'express';
 import { User } from '../../../../models/user.class';
 import { FirestoreService } from '../../../services/firestore.service';
+import { StorageService } from '../../../services/storage.service';
 
 @Component({
   selector: 'app-edit-profile-dialog',
@@ -16,11 +17,14 @@ import { FirestoreService } from '../../../services/firestore.service';
 export class EditProfileDialogComponent {
   constructor(private dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data:any) {}
 
+  downloadService = inject(StorageService)
   firestoreService = inject(FirestoreService)
   userId: any;
   actualUser: any = new User;
   nameValue: string | undefined;
   mailValue: string | undefined;
+
+  @ViewChild('profilePicture') profilePicture!: ElementRef;
 
   ngOnInit(){
     this.userId = this.data.userId;
@@ -31,7 +35,22 @@ export class EditProfileDialogComponent {
     }).catch((error) =>{
       console.error('Fehler beim abrufen der Benutzerdaten: ', error);
     });
-    // this.controlIfOwnPictureUsed(this.userId);
+    this.controlIfOwnPictureUsed(this.userId);
+  }
+
+
+  /**
+    * checks if user profile is a personal uploaded picture and download it from Firestore
+    * @param userID 
+    */
+  async controlIfOwnPictureUsed(userID:any){
+    if (this.actualUser.avatar === 'ownPictureDA'){
+    await  this.downloadService.downloadAvatar(userID);
+    }else if (this.profilePicture && this.profilePicture.nativeElement) {
+      this.profilePicture.nativeElement.src = this.actualUser.avatar;
+    } else {
+      console.error('Das Bild-Element wurde nicht richtig initialisiert.');
+    }
   }
 
   /**
