b471c4a5da3999256f286b8764306b2a98de3313
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-04-10 20:16:48 +0200

    group-chat: implement open edit dialog for edit channel informations

---
+++ src/app/dashboard/group-chat/edit-group-channel-dialog/edit-group-channel-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+
+@Component({
+  selector: 'app-edit-group-channel-dialog',
+  standalone: true,
+  imports: [MatCard],
+  templateUrl: './edit-group-channel-dialog.component.html',
+  styleUrl: './edit-group-channel-dialog.component.scss'
+})
+export class EditGroupChannelDialogComponent {
+
+}
--- src/app/dashboard/group-chat/group-chat.component.ts
+++ src/app/dashboard/group-chat/group-chat.component.ts
@@ -1,42 +1,54 @@
 import { Component, Input, Inject, inject, SimpleChanges } from '@angular/core';
 import { channelDataclientService } from '../../services/channelsDataclient.service';
 import { CommonModule } from '@angular/common';
-
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { EditGroupChannelDialogComponent } from './edit-group-channel-dialog/edit-group-channel-dialog.component';
 
 @Component({
   selector: 'app-group-chat',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './group-chat.component.html',
-  styleUrl: './group-chat.component.scss'
+  styleUrl: './group-chat.component.scss',
 })
 export class GroupChatComponent {
   @Input() currentId!: string;
-  chatService = inject(channelDataclientService)
+  chatService = inject(channelDataclientService);
   currentChannelData: any;
 
-  
+  constructor(public dialog: MatDialog) {}
+
   /**
    * This function checks if the id is changed by clicked on another channel in sidenav
    */
- async ngOnChanges(changes: SimpleChanges) {
+  async ngOnChanges(changes: SimpleChanges) {
     if (changes['currentId']) {
-     await this.loadCurrentDatas();
+      await this.loadCurrentDatas();
     }
   }
-
 
   /**
    * This function download the channeldatas from the channelService per id.
    */
   async loadCurrentDatas() {
     try {
-        const data = await this.chatService.getCurrentChannel(this.currentId);
-        this.currentChannelData = data;
-        console.log(this.currentChannelData);
-        
+      const data = await this.chatService.getCurrentChannel(this.currentId);
+      this.currentChannelData = data;
+      console.log(this.currentChannelData);
     } catch (error) {
-        console.error('Fehler beim Laden der Daten:', error);
+      console.error('Fehler beim Laden der Daten:', error);
     }
-}
+  }
+
+  /**
+   * open the edit dialog
+   */
+  openEditDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.panelClass = 'transparent-dialog';
+    dialogConfig.data = {
+      channelId: this.currentChannelData.id,
+    }
+    this.dialog.open(EditGroupChannelDialogComponent, dialogConfig);
+  }
 }
