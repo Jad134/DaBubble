e67a807e90fc40044e13fe3f6edc3c2eebacc88a
Jad El Nader <Jadelnader99@gmail.com>
2024-04-19 18:43:05 +0200

    member dialog

--- src/app/dashboard/group-chat/show-member-dialog/show-member-dialog.component.ts
+++ src/app/dashboard/group-chat/show-member-dialog/show-member-dialog.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatCard } from '@angular/material/card';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-show-member-dialog',
   standalone: true,
-  imports: [MatCard],
+  imports: [MatCard, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './show-member-dialog.component.html',
   styleUrl: './show-member-dialog.component.scss'
 })
 export class ShowMemberDialogComponent {
+  constructor(private dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  channelData: any;
+  users: any;
+
+  async ngOnInit() {
+    this.channelData = this.data.channelData
+    this.users = this.channelData.usersInChannel
+  }
+
 
 }
