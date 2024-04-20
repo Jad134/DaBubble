0263c0d7ef254abddc85673edc1130a76eda4335
Jad El Nader <Jadelnader99@gmail.com>
2024-04-20 17:11:39 +0200

    delete dead codes at add-channel.component

--- src/app/dashboard/sidenav-dashboard/add-channel/add-channel.component.ts
+++ src/app/dashboard/sidenav-dashboard/add-channel/add-channel.component.ts
@@ -65,17 +65,16 @@ export class AddChannelComponent {
     this.getIdFromURL()
   }
 
+
   ngAfterViewInit(): void {
     this.addChannelAdmin()
   }
+
 
   closeOverlay() {
     this.close.emit();
   }
 
-  // toggleUserOverlay() {
-  //   this.userOverlay = !this.userOverlay;
-  // }
 
   getIdFromURL() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -83,6 +82,7 @@ export class AddChannelComponent {
       this.currentUserId = id;
     }
   }
+
 
   addChannelAdmin() {
     const channelAdmin = this.users.find(user => user.id === this.currentUserId);
@@ -96,29 +96,6 @@ export class AddChannelComponent {
   }
 
 
-  chooseUser(userId: string) {
-    const userToAdd = this.users.filter(user => user.id === userId);
-    if (userToAdd.length > 0) {
-      this.selectedUser.push(...userToAdd);
-      console.log('Selected users:', this.selectedUser);
-    } else {
-      console.log('User not found with ID:', userId);
-    }
-    this.showUser()
-  }
-
-  addUsersToChannel() {
-    if (this.selectedOption === 'allUsers') {
-      this.selectedUser = [...this.users];
-      this.newChannel.usersInChannel = this.selectedUser;
-    } else if (this.selectedOption === 'specificPeople') {
-      this.newChannel.usersInChannel = this.selectedUser;
-    }
-    // Hier kann die Service funktion eingefügt werden 
-    console.log(this.newChannel);
-    this.channelDataclient.storeNewChannel(this.newChannel);
-  }
-
   checkFormChannelName() {
     const channelName = this.channelName.nativeElement;
     const validChannelName = this.validChannelName.nativeElement;
@@ -131,6 +108,7 @@ export class AddChannelComponent {
     }
   }
 
+  
   openDialog() {
     const dialogRef = this.dialog.open(AddUserChannelDialogComponent, {
       data: {
@@ -140,31 +118,5 @@ export class AddChannelComponent {
         users: this.users // Übergeben Sie die Benutzerdaten hier
       }
     });
-  }
-
-  showUser() {
-    if (this.currentName.trim() === '') {
-      // Wenn kein Suchbegriff vorhanden ist, alle Benutzer anzeigen, die nicht ausgewählt wurden
-      this.userList = this.users.filter(user => !this.selectedUser.some(selected => selected.id === user.id));
-    } else {
-      // Wenn ein Suchbegriff vorhanden ist, nach dem Suchbegriff filtern und dann nur die Benutzer anzeigen, die nicht ausgewählt wurden
-      this.userList = this.users.filter((user) => {
-        const userClean = user.name.replace(/\s/g, '');
-        const userCleanSmall = userClean.toLowerCase();
-        if (userCleanSmall.includes(this.currentName)) {
-          return user;
-        }
-      }).filter(user => !this.selectedUser.some(selected => selected.id === user.id));
-    }
-  }
-
-
-  /**
-   * This function removes the selected user for the Channel 
-   */
-  removeSelectedUser(userId: any) {
-    this.selectedUser = this.selectedUser.filter(user => user.id !== userId);
-    console.log('Selected users after removal:', this.selectedUser);
-
   }
 }
