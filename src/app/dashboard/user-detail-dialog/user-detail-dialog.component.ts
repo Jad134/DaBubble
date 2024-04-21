5cd16577e6e44f23ef276de262a15b233b0f4a4d
Tobias <112898220+Toby-22@users.noreply.github.com>
2024-04-21 14:00:25 +0200

    clean Code in user-detail-dialog

--- src/app/dashboard/user-detail-dialog/user-detail-dialog.component.ts
+++ src/app/dashboard/user-detail-dialog/user-detail-dialog.component.ts
@@ -2,11 +2,9 @@ import { Component, ElementRef, Inject, ViewChild, inject } from '@angular/core'
 import { MatCard } from '@angular/material/card';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { FirestoreService } from '../../services/firestore.service';
-import { StorageService } from '../../services/storage.service';
 import { User } from '../../../models/user.class';
 import { CommonModule } from '@angular/common';
 import { SharedServiceService } from '../../services/shared-service.service';
-import { user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-user-detail-dialog',
@@ -18,7 +16,6 @@ import { user } from '@angular/fire/auth';
 export class UserDetailDialogComponent {
   constructor(private dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data:any, private sharedService: SharedServiceService) {}
   firestoreService = inject(FirestoreService)
-  downloadService = inject(StorageService)
 
   userId = this.data.user.id;
   actualUser: any = new User;
@@ -28,21 +25,11 @@ export class UserDetailDialogComponent {
   async ngOnInit() {
     this.userId = this.data.user.id;
     this.usersInChannel = this.data.userInChannel.usersInChannel;
-    console.log("Aktuller User- User Dialog: ", this.actualUser, this.userId);
-    console.log(this.usersInChannel)
     this.usersInChannel.forEach(user => {
       if(user.id === this.userId){
         this.actualUser = user;
-      }else{
-        console.info("User nicht gefunden");
-      } 
+      }
     });
-    // this.userId = this.data.userId;
-    // await this.firestoreService.getUserDataById(this.userId).then((data) => {
-    //   this.actualUser = new User(data);
-    // }).catch((error) => {
-    //   console.error('Fehler beim abrufen der Benutzerdaten: ', error);
-    // });
   }
   
   /**
